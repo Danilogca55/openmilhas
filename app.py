0d5a2859b66030c88ba1bdd52cb796e4e947b31b
Danilogca55 <ddigiorgigomes37@icloud.com>
2026-03-08 19:24:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Esta linha procura o arquivo index.html dentro da pasta templates
+    # Tenta carregar o index.html da pasta templates
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # O Render usa a variável de ambiente PORT, isso evita o erro 502
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
