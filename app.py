dbd5c857f9c0811a48d0acb98590c92365cab660
Danilogca55 <ddigiorgigomes37@icloud.com>
2026-03-08 19:48:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Isso vai abrir o formulário de cadastro que criamos
-    return render_template('index.html')
+    # Criamos um "info" básico para o site não travar ao carregar o HTML
+    info = {
+        "usuario": "Danilo"
+    }
+    return render_template('index.html', info=info)
 
 if __name__ == '__main__':
     # Configuração obrigatória para o Render não dar erro 500
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
