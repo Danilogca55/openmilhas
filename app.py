643da127a8f2c19e0b4ec9a676b768db0eb4235b
Danilogca55 <ddigiorgigomes37@icloud.com>
2026-03-08 19:29:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Tenta carregar o index.html da pasta templates
+    # Isso vai abrir o formulário de cadastro que criamos
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # O Render usa a variável de ambiente PORT, isso evita o erro 502
+    # Configuração obrigatória para o Render não dar erro 500
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
